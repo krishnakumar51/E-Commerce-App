c79da2e1c39ef4bee0a338ef798cc8756ebdd123
krishnakumar51 <godkrishnasskal@gmail.com>
2024-03-04 03:27:48 +0530

    Added the Search bar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/commmon/widgets/appbar/appbar.dart';
-import 'package:t_store/commmon/widgets/cutom_shapes/curved_edges/primary_header_container.dart';
+import 'package:t_store/commmon/widgets/cutom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/commmon/widgets/cutom_shapes/containers/search_container.dart';
 import 'package:t_store/commmon/widgets/products_cart/cart_menu_icon.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_app_bar.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/device/device_utilities.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -19,6 +24,15 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 children: [
                   THomeAppBar(),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  TSearchContainer(
+                    text: "Search in Store",
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
                 ],
               ),
             ),
